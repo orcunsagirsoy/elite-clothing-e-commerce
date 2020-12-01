4aece9ac5d208c37dcd19651aa74c858e14d8b68
orcunsagirsoy <orcunsagirsoy@gmail.com>
2020-12-01 14:41:54 +0300

    create project with create react app and addfiles for homepage

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import HomePage from "./components/HomePage/HomePage"
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
