ba6fc14c4b0c85c69d8fe7dd16972e6e774505bb
orcunsagirsoy <orcunsagirsoy@gmail.com>
2020-12-02 23:13:14 +0300

    add withRouter to have dynamic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import HomePage from "./pages/homepage/HomePage"
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import React from 'react';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
